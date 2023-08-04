e7e933edff5d016661ed43aa9e273a376855dd8d
luciancahil <royhe62@yahoo.ca|>
2023-08-04 10:36:32 -0700

    Fixed version num

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="iCypress",
-    version="0.3",
+    version="0.4",
     author="royhe",
     package_data={'iCYPRESS': ['configs/example_custom.yaml']},
     author_email="royhe62@yahoo.ca",
